04c886cd6f0e718472044e7d0b625a0af426172f
Jack Lewis <jlapex.aeon@gmail.com>
2017-06-20 10:06:11 -0500

    Entity system! Doors that teleport you! Also, I seem to have done something that makes it immensely faster??? Like too fast...

--- Cataclysm-SRC/scripts/game.py
+++ Cataclysm-SRC/scripts/game.py
@@ -1,11 +1,19 @@
 import pygame, sys
 from pygame.locals import *
 from common import DISPLAYSURF
+import json
 
 gs = {'isJumping':False,'jumpHeight':0,'isMovingUp':False,'isMovingDown':False,'isMovingLeft':False,'isMovingRight':False,'char':'../assets/sprites/player/player.png', 'x':0, 'y':0 ,'z':0, 'lvl':'../maps/c01a', 'realX':300, 'realY':300}
 pygame.font.init()
 FONT = pygame.font.SysFont('Bauhaus 93 Regular', 40)
-
+def tickDoor(obj): # For every door object in a level, this will run, with the specified parameters of each specific door.
+    if gs['x'] >= obj['posdict']['x'] and gs['y'] >= obj['posdict']['y'] and gs['z'] >= obj['posdict']['z'] and gs['x'] <= obj['dposdict']['x'] and gs['y'] <= obj['dposdict']['y'] and gs['z'] <= obj['dposdict']['z']:
+        # Take the player to the destination if they are within the boundaries of the door.
+        gs['x'] = 0
+        gs['y'] = 0
+        gs['z'] = 0
+        gs['realX'] = 300
+        gs['realY'] = 300
 def getGamestate(): # Will be used for saving.
     return gs
 
@@ -18,17 +26,22 @@ def start():
     charflip = pygame.transform.flip(char, True, False)
     chardisplay = char
     lvl = pygame.image.load(gs['lvl'] + '/visual.png')
+    entities = json.loads(open(gs['lvl'] + '/entities.json').read())
     charmask = pygame.mask.from_surface(char)
     lvlmask = pygame.mask.from_surface(pygame.image.load(gs['lvl'] + '/walls.png'))
     bullet = pygame.image.load('../assets/sprites/bullet/bullet.png')
     bx = 0
     by = 0
     bulletIsExisting = False
-    hitbox = pygame.image.load('../assets/sprites/player/hitbox.png')
-    hitmask = pygame.mask.from_surface(hitbox)
+    hitmask = pygame.mask.from_surface(pygame.image.load('../assets/sprites/player/hitbox.png'))
     while True: 
         DISPLAYSURF.blit(lvl, (0,0))
         DISPLAYSURF.blit(chardisplay,(gs['realX'],gs['realY']))
+
+        for obj in entities: # Tick through every entity in the lvl
+            if obj['type'] == 'door':
+                tickDoor(obj)
+        
         if bulletIsExisting:
             DISPLAYSURF.blit(bullet,(bx,by))
             if bulletFacing is 'left':
@@ -111,7 +124,7 @@ def start():
                 gs['jumpHeight'] = 0
                 gs['isJumping'] = 0
 
-        DISPLAYSURF.blit(FONT.render(str(gs['x']) + '_' + str(gs['z']), True, (0, 128, 255), (0, 0, 0)), (25,25))
+        DISPLAYSURF.blit(FONT.render(str(gs['x']) + '_' + str(gs['y']) + '_' + str(gs['z']), True, (0, 128, 255), (0, 0, 0)), (25,25))
 
         pygame.display.update()
 
